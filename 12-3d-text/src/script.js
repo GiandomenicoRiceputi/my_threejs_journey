6ca2194092cd3ec5083e8dd14f49df3c3d336468
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2024-02-24 14:58:08 +0100

    some fun 12

--- 12-3d-text/src/script.js
+++ 12-3d-text/src/script.js
@@ -1,11 +1,17 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
-import GUI from "lil-gui";
+import typefaceFont from "../static/fonts/helvetiker_regular.typeface.json";
 
 // Constants
 const DONUT_COUNT = 100;
+const DONUT_SIZE = 0.3;
+const DONUT_THICKNESS = 0.2;
+const DONUT_SEGMENTS = 32;
+const DONUT_RINGS = 64;
+const ROTATION_SPEED = 0.003; // Add this line
 
 // Debug
 const gui = new GUI();
@@ -18,85 +24,17 @@ const scene = new THREE.Scene();
 
 // Textures
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/8.png");
-matcapTexture.encoding = THREE.sRGBEncoding;
+const matcapTexture = textureLoader.load("textures/matcaps/1.png");
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 // Fonts
 const fontLoader = new FontLoader();
-
-// Create an array to store the donuts and the letters
-let donuts = [];
-let letters = [];
-
-// Create donut
-function createDonut(geometry, material) {
-  const donut = new THREE.Mesh(geometry, material);
-  donut.position.x = (Math.random() - 0.5) * 10;
-  donut.position.y = (Math.random() - 0.5) * 10;
-  donut.position.z = (Math.random() - 0.5) * 10;
-  donut.rotation.x = Math.random() * Math.PI;
-  donut.rotation.y = Math.random() * Math.PI;
-  const scale = Math.random();
-  donut.scale.set(scale, scale, scale);
-  scene.add(donut);
-  donuts.push(donut); // Add the donut to the array
-}
-
-fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-
-  for (let i = 0; i < DONUT_COUNT; i++) {
-    createDonut(donutGeometry, material);
-  }
-
-  const text = "Iris Rocks!";
-  const letterPositions = Array.from(text).map(
-    (_, i) => i - (text.length / 2) * 0.6,
-  );
-
-  Array.from(text).forEach((char, i) => {
-    const letterGeometry = new TextGeometry(char, {
-      font: font,
-      size: 0.5,
-      height: 0.2,
-      curveSegments: 3,
-      bevelEnabled: true,
-      bevelThickness: 0.03,
-      bevelSize: 0.02,
-      bevelOffset: 0,
-      bevelSegments: 2,
-    });
-
-    letterGeometry.computeBoundingBox();
-    letterGeometry.center();
-
-    const letter = new THREE.Mesh(letterGeometry, material);
-    letter.position.x = letterPositions[i];
-    scene.add(letter);
-    letters.push(letter);
-  });
-});
 
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -118,33 +56,85 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.setClearColor(0xffffff);
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
+// Clock
 const clock = new THREE.Clock();
+
+// Donuts array
+let donuts = [];
+
+// Functions
+function createDonut(material) {
+  const donutGeometry = new THREE.TorusGeometry(
+    DONUT_SIZE,
+    DONUT_THICKNESS,
+    DONUT_SEGMENTS,
+    DONUT_RINGS,
+  );
+  const donut = new THREE.Mesh(donutGeometry, material);
+  donut.position.x = (Math.random() - 0.5) * 10;
+  donut.position.y = (Math.random() - 0.5) * 10;
+  donut.position.z = (Math.random() - 0.5) * 10;
+  donut.rotation.x = Math.random() * Math.PI;
+  donut.rotation.y = Math.random() * Math.PI;
+  const scale = Math.random();
+  donut.scale.set(scale, scale, scale);
+  return donut;
+}
+
+function createText(material, font) {
+  const textGeometry = new TextGeometry("Iris Is Beautifull", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  textGeometry.center();
+  const text = new THREE.Mesh(textGeometry, material);
+  return text;
+}
+
+function animateDonuts() {
+  donuts.forEach((donut) => {
+    donut.rotation.x += ROTATION_SPEED;
+    donut.rotation.y += ROTATION_SPEED;
+  });
+}
+
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const text = createText(material, font);
+  scene.add(text);
+
+  for (let i = 0; i < DONUT_COUNT; i++) {
+    const donut = createDonut(material);
+    scene.add(donut);
+    donuts.push(donut);
+  }
+});
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Animate donuts
-  for (let donut of donuts) {
-    donut.rotation.x += Math.sin(elapsedTime) * 0.01;
-    donut.rotation.y += Math.cos(elapsedTime) * 0.01;
-  }
-
-  // Animate letters
-  for (let letter of letters) {
-    letter.rotation.y += 0.01;
-  }
-
-  // Update controls
   controls.update();
-
-  // Render
+  animateDonuts();
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
